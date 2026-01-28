042150fadfefc78bc13b7990af2728881e6188d8
Tom <tom.razinkov@gmail.com>
2026-01-28 16:49:25 +0200

    Added heuristic agent and rewritten part of the code to be more readable

--- game.py
+++ game.py
@@ -6,6 +6,14 @@ import os
 
 
 class FourInASquareGame:
+    # Class-level dictionaries - separate learning from saving
+    learning_boards = {}  # Loaded from load_file for reference
+    new_boards = {}  # Only new boards from this tournament
+    
+    # Statistics for greedy agent performance
+    total_greedy_moves = 0
+    random_fallback_moves = 0
+    
     def __init__(self, play_mode="RANDOM", load_json_file="boards_and_scores.json", save_json_file=None):
         self.board_state = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
         self.empty_spots = [[0, 1, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3], [], [0, 1, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3]]
@@ -15,12 +23,12 @@ class FourInASquareGame:
         self.load_json_file = load_json_file
         self.save_json_file = save_json_file if save_json_file else load_json_file
 
-        # Load from the specified file for learning
-        if os.path.exists(self.load_json_file):
+        # Load learning data once
+        if not FourInASquareGame.learning_boards and os.path.exists(self.load_json_file):
             with open(self.load_json_file, "r") as f:
-                self.boards_and_scores = json.load(f)
-        else:
-            self.boards_and_scores = {}
+                FourInASquareGame.learning_boards = json.load(f)
+        
+        self.boards_and_scores = FourInASquareGame.learning_boards
         
 
     def score_boards(self):
@@ -40,27 +48,37 @@ class FourInASquareGame:
 
 
     def save_game_to_dict(self):
-        # First score the boards from this game
         self.score_boards()
         
-        # Load existing save file data (might be different from load file)
+        # Update new_boards dictionary (tournament session only)
+        for key, value in self.game_boards.items():
+            if key in FourInASquareGame.new_boards:
+                num, avg = FourInASquareGame.new_boards[key]
+                new_avg = ((avg * num) + value) / (num + 1)
+                FourInASquareGame.new_boards[key] = (num + 1, new_avg)
+            else:
+                FourInASquareGame.new_boards[key] = (1, value)
+    
+    @staticmethod
+    def save_all_to_file(filename):
+        """Call this once at the end to save everything"""
+        # Load existing save file
         save_data = {}
-        if os.path.exists(self.save_json_file):
-            with open(self.save_json_file, "r") as f:
+        if os.path.exists(filename):
+            with open(filename, "r") as f:
                 save_data = json.load(f)
         
-        # Add scored boards to save data with running averages
-        for key, value in self.game_boards.items():
+        # Merge new boards with existing save data
+        for key, value in FourInASquareGame.new_boards.items():
             if key in save_data:
-                num_of_appearances = save_data[key][0]
-                num_of_appearances += 1
-                avg_score = ((save_data[key][1] * (num_of_appearances - 1)) + value) / num_of_appearances
-                save_data[key] = (num_of_appearances, avg_score)
+                num, avg = save_data[key]
+                new_num, new_avg = value
+                merged_avg = ((avg * num) + (new_avg * new_num)) / (num + new_num)
+                save_data[key] = (num + new_num, merged_avg)
             else:
-                save_data[key] = (1, value)
+                save_data[key] = value
         
-        # Save to the save JSON file
-        with open(self.save_json_file, "w") as f:
+        with open(filename, "w") as f:
             json.dump(save_data, f)
 
 
@@ -74,6 +92,34 @@ class FourInASquareGame:
                 for cell in sub_board:
                     board_state_string += "1" if cell == 1 else "2" if cell == 2 else "0"
         return board_state_string
+    
+    @staticmethod
+    def board_to_6x6(board_state):
+        """Convert 9 sub-boards (3x3 grid of 2x2 boards) into one 6x6 array."""
+        board_6x6 = np.zeros((6, 6), dtype=int)
+        
+        for i in range(9):
+            # Determine which 2x2 section of the 6x6 board this sub-board occupies
+            sub_board_row = i // 3  # 0, 1, or 2
+            sub_board_col = i % 3   # 0, 1, or 2
+            
+            # Starting position in the 6x6 array
+            start_row = sub_board_row * 2
+            start_col = sub_board_col * 2
+            
+            # Fill in the 2x2 section
+            if board_state[i] != []:
+                board_6x6[start_row][start_col] = board_state[i][0]
+                board_6x6[start_row][start_col + 1] = board_state[i][1]
+                board_6x6[start_row + 1][start_col] = board_state[i][2]
+                board_6x6[start_row + 1][start_col + 1] = board_state[i][3]
+            else:
+                board_6x6[start_row][start_col] = 0
+                board_6x6[start_row][start_col + 1] = 0
+                board_6x6[start_row + 1][start_col] = 0
+                board_6x6[start_row + 1][start_col + 1] = 0
+        
+        return board_6x6
     
 
     def refresh_sub_board_spots(self, source_idx=None, destination_idx=None):
@@ -167,17 +213,14 @@ class FourInASquareGame:
                                 best_sub_board_to_move = j
 
             if best_move is None and best_sub_board_to_move is None:
+                FourInASquareGame.random_fallback_moves += 1
+                FourInASquareGame.total_greedy_moves += 1
                 self.perform_random_agent_move()
                 
             else:
-                empty_sub_board_spot = self.possible_sub_board_spots.index(2)
-                self.board_state[best_move[0]][best_move[1]] = 1
-                self.empty_spots[best_move[0]].remove(best_move[1])
-                self.board_state[empty_sub_board_spot] = copy.deepcopy(self.board_state[best_sub_board_to_move])
-                self.empty_spots[empty_sub_board_spot] = copy.deepcopy(self.empty_spots[best_sub_board_to_move])
-                self.board_state[best_sub_board_to_move] = []
-                self.empty_spots[best_sub_board_to_move] = []
-                self.refresh_sub_board_spots(best_sub_board_to_move, empty_sub_board_spot)
+                FourInASquareGame.total_greedy_moves += 1
+                move = (best_move[0], best_move[1], best_sub_board_to_move)
+                self.execute_move(move, player_value=1)
 
 
     def reset_game(self):
@@ -219,18 +262,11 @@ class FourInASquareGame:
         place_sub_board_idx = random.choice(non_empty_indices)
         random_spot = random.choice(self.empty_spots[place_sub_board_idx])
 
-        self.board_state[place_sub_board_idx][random_spot] = player_value
-        self.empty_spots[place_sub_board_idx].remove(random_spot)
-
         available_sub_board_indices = [i for i, val in enumerate(self.possible_sub_board_spots) if val == 1]
         move_sub_board_idx = random.choice(available_sub_board_indices)
-
-        empty_sub_board_spot = self.possible_sub_board_spots.index(2)
-        self.board_state[empty_sub_board_spot] = copy.deepcopy(self.board_state[move_sub_board_idx])
-        self.empty_spots[empty_sub_board_spot] = copy.deepcopy(self.empty_spots[move_sub_board_idx])
-        self.board_state[move_sub_board_idx] = []
-        self.empty_spots[move_sub_board_idx] = []
-        self.refresh_sub_board_spots(move_sub_board_idx, empty_sub_board_spot)
+        
+        move = (place_sub_board_idx, random_spot, move_sub_board_idx)
+        self.execute_move(move, player_value)
 
     def perform_random_rival_move(self):
         self.make_random_move(2)
@@ -238,52 +274,111 @@ class FourInASquareGame:
     def perform_random_agent_move(self):
         self.make_random_move(1)
 
-    def check_win(self):
-        b = self.board_state
-
-        # Convert board value to text
-        def player_to_text(val):
-            return "Red" if val == 1 else "White"
-        
-        # Helper function to get value at a position in the full 6x6 grid
-        def get_position(global_row, global_col):
-            # Each sub-board is 2x2, arranged in a 3x3 grid of sub-boards
-            sub_board_row = global_row // 2  # Which row of sub-boards (0, 1, or 2)
-            sub_board_col = global_col // 2  # Which column of sub-boards (0, 1, or 2)
-            sub_board_idx = sub_board_row * 3 + sub_board_col
-            
-            # Check if this sub-board is empty
-            if b[sub_board_idx] == []:
-                return 0  # Empty position
-            
-            # Position within the 2x2 sub-board
-            pos_row = global_row % 2
-            pos_col = global_col % 2
-            pos_idx = pos_row * 2 + pos_col
-            
-            return b[sub_board_idx][pos_idx]
-        
-        # Check all possible 2x2 squares in the 6x6 grid
-        # There are 5x5 = 25 possible 2x2 squares
-        for row in range(5):
-            for col in range(5):
-                # Get the 2x2 square starting at (row, col)
-                top_left = get_position(row, col)
-                top_right = get_position(row, col + 1)
-                bottom_left = get_position(row + 1, col)
-                bottom_right = get_position(row + 1, col + 1)
+    def find_winning_move(self):
+        """Find a move that wins the game for player 1."""
+        for i in range(9):
+            if self.board_state[i] == []:
+                continue
                 
-                # Check if all four positions have the same non-zero value
-                if (top_left == top_right == bottom_left == bottom_right != 0):
-                    return f"{player_to_text(top_left)} wins"
+            for spot in self.empty_spots[i]:
+                possible_move = copy.deepcopy(self.board_state)
+                possible_move[i][spot] = 1
+                
+                for j in range(9):
+                    if self.possible_sub_board_spots[j] == 1:
+                        empty_sub_board_spot = self.possible_sub_board_spots.index(2)
+                        possible_board = copy.deepcopy(possible_move)
+                        
+                        possible_board[empty_sub_board_spot] = copy.deepcopy(possible_board[j])
+                        possible_board[j] = []
+                        
+                        board_6x6 = FourInASquareGame.board_to_6x6(possible_board)
+                        
+                        for row in range(5):
+                            for col in range(5):
+                                block = board_6x6[row:row+2, col:col+2]
+                                if np.all(block == 1):
+                                    return (i, spot, j)
+        return None
+    
+    def find_blocking_move(self):
+        """Find a move that blocks opponent from winning."""
+        for i in range(9):
+            if self.board_state[i] == []:
+                continue
+                
+            for spot in self.empty_spots[i]:
+                opponent_move = copy.deepcopy(self.board_state)
+                opponent_move[i][spot] = 2
+                
+                for j in range(9):
+                    if self.possible_sub_board_spots[j] == 1:
+                        empty_sub_board_spot = self.possible_sub_board_spots.index(2)
+                        opponent_board = copy.deepcopy(opponent_move)
+                        
+                        opponent_board[empty_sub_board_spot] = copy.deepcopy(opponent_board[j])
+                        opponent_board[j] = []
+                        
+                        board_6x6_opp = FourInASquareGame.board_to_6x6(opponent_board)
+                        
+                        for row in range(5):
+                            for col in range(5):
+                                block = board_6x6_opp[row:row+2, col:col+2]
+                                if np.all(block == 2):
+                                    return (i, spot, j)
+        return None
+    
+    def execute_move(self, move, player_value=1):
+        """Execute a move given as (sub_board_idx, spot_idx, sub_board_to_move)."""
+        sub_board_idx, spot_idx, sub_board_to_move = move
+        empty_sub_board_spot = self.possible_sub_board_spots.index(2)
+        
+        self.board_state[sub_board_idx][spot_idx] = player_value
+        self.empty_spots[sub_board_idx].remove(spot_idx)
+        self.board_state[empty_sub_board_spot] = copy.deepcopy(self.board_state[sub_board_to_move])
+        self.empty_spots[empty_sub_board_spot] = copy.deepcopy(self.empty_spots[sub_board_to_move])
+        self.board_state[sub_board_to_move] = []
+        self.empty_spots[sub_board_to_move] = []
+        self.refresh_sub_board_spots(sub_board_to_move, empty_sub_board_spot)
 
-        # Draw? (no empty spots in any non-empty sub-board)
-        if not any(0 in sub_board for sub_board in b if sub_board != []):
-            return "Draw"
+    def perform_heuristic_agent_move(self):
+        """Make a move using heuristics: win > block > random/greedy."""
+        # Priority 1: Find a winning move
+        winning_move = self.find_winning_move()
+        if winning_move:
+            self.execute_move(winning_move)
+            return
+        
+        # Priority 2: Block opponent's winning move
+        blocking_move = self.find_blocking_move()
+        if blocking_move:
+            self.execute_move(blocking_move)
+            return
+        
+        # Priority 3: 50/50 between random and greedy
+        if random.random() < 0.5:
+            self.perform_random_agent_move()
+        else:
+            self.perform_greedy_agent_move()
 
-        # Still playing
-        return "Ongoing"
 
+    def check_win(self):
+        board_6x6 = FourInASquareGame.board_to_6x6(self.board_state)
+        
+        for i in range(5):
+            for j in range(5):
+                # Check 2x2 block
+                block = board_6x6[i:i+2, j:j+2]
+                if np.all(block == 1):
+                    return "Red wins"
+                elif np.all(block == 2):
+                    return "White wins"
+                
+        if any(spot != [] for spot in self.empty_spots):
+            return "Ongoing"
+        
+        return "Draw"
+            
 
     def print_board(self):
         # ANSI color codes
--- tournament.py
+++ tournament.py
@@ -9,24 +9,47 @@ class Tournament:
 
     def __init__(self):
         self.games_number = int(input("How many games do you want to play? "))
-        self.play_mode = input("RANDOM or GREEDY: ").strip().upper()
+        print("\nChoose mode:")
+        print("1. Random")
+        print("2. Greedy on random json (boards_and_scores.json)")
+        print("3. Greedy on greedy json (greedy_boards_and_scores.json)")
+        choice = input("Enter choice (1, 2, or 3): ").strip()
+        
         self.stats_dict = {"Red wins": 0, "White wins": 0, "Draw": 0}
         
-        # Set up file paths based on mode
-        if self.play_mode == "GREEDY":
+        # Set up play mode and file paths based on choice
+        if choice == "1":
+            self.play_mode = "RANDOM"
+            self.load_file = "boards_and_scores.json"
+            self.save_file = "boards_and_scores.json"
+        elif choice == "2":
+            self.play_mode = "GREEDY"
             self.load_file = "boards_and_scores.json"
             self.save_file = "greedy_boards_and_scores.json"
+        elif choice == "3":
+            self.play_mode = "GREEDY"
+            self.load_file = "greedy_boards_and_scores.json"
+            self.save_file = "greedy_boards_and_scores.json"
         else:
+            print("Invalid choice, defaulting to Random")
+            self.play_mode = "RANDOM"
             self.load_file = "boards_and_scores.json"
             self.save_file = "boards_and_scores.json"
 
     def run_games(self):
-        # Count initial boards
+        # Load initial boards from save file only
         try:
             with open(self.save_file, "r") as f:
-                initial_boards = len(json.load(f))
+                initial_save_data = json.load(f)
+                initial_boards = len(initial_save_data)
         except FileNotFoundError:
             initial_boards = 0
+        
+        # Clear new_boards and load the learning data
+        FourInASquareGame.new_boards = {}
+        FourInASquareGame.total_greedy_moves = 0
+        FourInASquareGame.random_fallback_moves = 0
+        dummy = FourInASquareGame(self.play_mode, load_json_file=self.load_file, save_json_file=self.save_file)
         
         for i in range(self.games_number):
             if (i + 1) % 1000 == 0 or i == 0:
@@ -41,14 +64,16 @@ class Tournament:
             
             game.save_game_to_dict()
         
-        # Count final boards and calculate new boards learned
-        try:
-            with open(self.save_file, "r") as f:
-                final_boards = len(json.load(f))
-        except FileNotFoundError:
-            final_boards = 0
+        # Save once at the end
+        FourInASquareGame.save_all_to_file(self.save_file)
+        
+        # Count final boards from save file
+        with open(self.save_file, "r") as f:
+            final_save_data = json.load(f)
+            final_boards = len(final_save_data)
         
         self.new_boards_learned = final_boards - initial_boards
+        self.total_boards = final_boards
 
     def print_tournament_result(self):
         print(f"\nResults for {self.games_number} games ({self.play_mode} mode):")
@@ -56,13 +81,14 @@ class Tournament:
         print(f"White wins: {self.stats_dict['White wins']} ({self.stats_dict['White wins'] / self.games_number * 100:.1f}%)")
         print(f"Draws: {self.stats_dict['Draw']} ({self.stats_dict['Draw'] / self.games_number * 100:.1f}%)")
         
-        try:
-            with open(self.save_file, "r") as f:
-                boards_and_scores = json.load(f)
-            print(f"\nTotal board states in {self.save_file}: {len(boards_and_scores)}")
-            print(f"New boards learned during this tournament: {self.new_boards_learned}")
-        except FileNotFoundError:
-            print(f"\nNo board states saved yet in {self.save_file}.")
+        print(f"\nTotal board states in {self.save_file}: {self.total_boards}")
+        print(f"New boards learned during this tournament: {self.new_boards_learned}")
+        
+        if self.play_mode == "GREEDY" and FourInASquareGame.total_greedy_moves > 0:
+            fallback_percentage = (FourInASquareGame.random_fallback_moves / FourInASquareGame.total_greedy_moves) * 100
+            print(f"\nGreedy Agent Statistics:")
+            print(f"Total greedy moves attempted: {FourInASquareGame.total_greedy_moves}")
+            print(f"Random fallback moves (board not found): {FourInASquareGame.random_fallback_moves} ({fallback_percentage:.2f}%)")
 
 
 if __name__ == "__main__":
